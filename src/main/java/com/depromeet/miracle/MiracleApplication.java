2ca674068dadea7629b60c57e720405a16706314
하재권 <hazxz@naver.com>
2020-07-22 01:10:48 +0900

    initialize spring boot project

---
+++ src/main/java/com/depromeet/miracle/MiracleApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.miracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiracleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiracleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/miracle/MiracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.miracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiracleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
