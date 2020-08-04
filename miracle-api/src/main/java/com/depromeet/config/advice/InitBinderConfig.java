42f215db651cea26c685a28434c4f3b12a57b22f
seungh0 <will.seungho@gmail.com>
2020-08-04 21:10:43 +0900

    [Miracle-4] SetUp InitBinder

---
+++ miracle-api/src/main/java/com/depromeet/config/advice/InitBinderConfig.java
@@ -0,0 +1,15 @@
+package com.depromeet.config.advice;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class InitBinderConfig {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+    }
+
+}
