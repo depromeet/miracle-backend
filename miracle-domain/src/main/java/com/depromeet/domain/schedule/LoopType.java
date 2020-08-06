965fdc41d28b783a69e3c389f799be1dbf454645
하재권 <hazxz@naver.com>
2020-08-06 12:30:51 +0900

    feat: Schedule Entity 클래스 추가

---
+++ miracle-domain/src/main/java/com/depromeet/domain/schedule/LoopType.java
@@ -0,0 +1,23 @@
+package com.depromeet.domain.schedule;
+
+import java.util.Arrays;
+
+public enum LoopType {
+    NONE("NONE"),
+    DAY("DAY"),
+    WEEK("WEEK"),
+    MONTH("MONTH");
+
+    private final String text;
+
+    LoopType(String text) {
+        this.text = text;
+    }
+
+    public static LoopType of(String text) {
+        return Arrays.stream(values())
+            .filter(dayOfWeek -> dayOfWeek.text.equals(text))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(String.format("Parameter is invalid. %s", text)));
+    }
+}
