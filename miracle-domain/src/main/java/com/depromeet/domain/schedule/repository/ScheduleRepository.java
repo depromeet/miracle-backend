708eac1e50f19617af28377bf804fe36dbb3c1a1
하재권 <hazxz@naver.com>
2020-08-06 12:31:27 +0900

    feat: Schedule Entity 조회를 위한 Repository 추가

---
+++ miracle-domain/src/main/java/com/depromeet/domain/schedule/repository/ScheduleRepository.java
@@ -0,0 +1,9 @@
+package com.depromeet.domain.schedule.repository;
+
+import com.depromeet.domain.schedule.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
