03fb7847735266118c888d5156883c1caad806b3
하재권 <hazxz@naver.com>
2020-08-10 09:43:24 +0900

    refactor: ScheduleTest 코드 보완, 리팩토링

--- miracle-domain/src/test/java/com/depromeet/domain/schedule/ScheduleTest.java
+++ miracle-domain/src/test/java/com/depromeet/domain/schedule/ScheduleTest.java
@@ -18,7 +18,7 @@ class ScheduleTest {
     @DisplayName("Schedule 객체를 생성할 수 있다")
     @ParameterizedTest
     @MethodSource("source_createSchedule_ShouldSuccess")
-    public void createSchedule_ShouldSuccess(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
+    void createSchedule_ShouldSuccess(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
         Schedule schedule = Schedule.of(memberId, startTime, endTime, category, description, loopType);
         assertAll(
             () -> assertThat(schedule.getYear()).isEqualTo(startTime.getYear()),
@@ -30,24 +30,24 @@ class ScheduleTest {
         );
     }
 
-    public static Stream<Arguments> source_createSchedule_ShouldSuccess() {
+    static Stream<Arguments> source_createSchedule_ShouldSuccess() {
         return Stream.of(
             Arguments.of(1L, LocalDateTime.of(2020, 8, 6, 8, 0, 0), LocalDateTime.of(2020, 8, 6, 8, 30, 0), "운동", "운동하기", "NONE"),
-            Arguments.of(1L, LocalDateTime.of(2020, 8, 6, 9, 0, 0), LocalDateTime.of(2020, 8, 6, 10, 0, 0), "운동", "스트레칭", "DAY"),
-            Arguments.of(1L, LocalDateTime.of(2020, 8, 6, 11, 0, 0), LocalDateTime.of(2020, 8, 6, 11, 30, 0), "수면", "취침시간", "WEEK")
+            Arguments.of(2L, LocalDateTime.of(2020, 8, 6, 9, 0, 0), LocalDateTime.of(2020, 8, 6, 10, 0, 0), "요가", "스트레칭", "DAY"),
+            Arguments.of(3L, LocalDateTime.of(2020, 8, 6, 22, 0, 0), LocalDateTime.of(2020, 8, 6, 22, 30, 0), "수면", "취침시간", "WEEK")
         );
     }
 
     @DisplayName("시작시간이 종료시간 이후라면 예외 발생")
     @ParameterizedTest
     @MethodSource("source_startTimeAfterEndTime_ShouldFail")
-    public void startTimeAfterEndTime_ShouldFail(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
+    void startTimeAfterEndTime_ShouldFail(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
         assertThatThrownBy(() -> {
             Schedule.of(memberId, startTime, endTime, category, description, loopType);
         }).isInstanceOf(InvalidScheduleTimeException.class);
     }
 
-    public static Stream<Arguments> source_startTimeAfterEndTime_ShouldFail() {
+    static Stream<Arguments> source_startTimeAfterEndTime_ShouldFail() {
         return Stream.of(
             Arguments.of(1L, LocalDateTime.of(2020, 8, 6, 8, 30, 10), LocalDateTime.of(2020, 8, 6, 8, 30, 0), "운동", "운동하기", "NONE"),
             Arguments.of(1L, LocalDateTime.of(2020, 8, 6, 10, 10, 0), LocalDateTime.of(2020, 8, 6, 10, 0, 0), "운동", "스트레칭", "DAY"),
@@ -58,13 +58,13 @@ class ScheduleTest {
     @DisplayName("시작시간과 종료시간이 같은 날짜가 아니라면 예외 발생")
     @ParameterizedTest
     @MethodSource("source_startTimeAndEndTimeNotSame_ShouldFail")
-    public void startTimeAndEndTimeNotSame_ShouldFail(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
+    void startTimeAndEndTimeNotSame_ShouldFail(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
         assertThatThrownBy(() -> {
             Schedule.of(memberId, startTime, endTime, category, description, loopType);
         }).isInstanceOf(InvalidScheduleTimeException.class);
     }
 
-    public static Stream<Arguments> source_startTimeAndEndTimeNotSame_ShouldFail() {
+    static Stream<Arguments> source_startTimeAndEndTimeNotSame_ShouldFail() {
         return Stream.of(
             Arguments.of(1L, LocalDateTime.of(2020, 8, 7, 8, 0, 0), LocalDateTime.of(2020, 8, 6, 8, 30, 0), "운동", "운동하기", "NONE"),
             Arguments.of(1L, LocalDateTime.of(2020, 9, 6, 9, 0, 0), LocalDateTime.of(2020, 8, 6, 10, 0, 0), "운동", "스트레칭", "DAY"),
