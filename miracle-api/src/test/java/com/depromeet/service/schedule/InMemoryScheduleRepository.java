1c89b62496284913dddff269564900f16d68fbfd
하재권 <hazxz@naver.com>
2020-08-10 10:35:22 +0900

    refactor: SchdueleService Test 를 Spring 통합 테스트로 수행하지 않기 위해, H2 Database 와의 의존성을 끊고 ScheduleRepository 의 구현체(테스트용도) InMemoryScheduleRepository 를 추가

---
+++ miracle-api/src/test/java/com/depromeet/service/schedule/InMemoryScheduleRepository.java
@@ -0,0 +1,213 @@
+package com.depromeet.service.schedule;
+
+import com.depromeet.domain.schedule.LoopType;
+import com.depromeet.domain.schedule.Schedule;
+import com.depromeet.domain.schedule.ScheduleRepository;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.lang.reflect.Field;
+import java.time.DayOfWeek;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class InMemoryScheduleRepository implements ScheduleRepository {
+
+    public static final long MEMBER_1 = 1L;
+    public static final long MEMBER_2 = 2L;
+    private final List<Schedule> schedules = new ArrayList<>();
+    private long currentId = 1L;
+
+    public InMemoryScheduleRepository() {
+        schedules.add(generateSchedule(MEMBER_1, LocalDateTime.of(2020, 8, 8, 6, 0), LocalDateTime.of(2020, 8, 8, 6, 30), "기상", "기상하기", LoopType.NONE.getText()));
+        schedules.add(generateSchedule(MEMBER_1, LocalDateTime.of(2020, 8, 8, 6, 0), LocalDateTime.of(2020, 8, 8, 6, 30), "기상", "기상하기", LoopType.DAY.getText()));
+        schedules.add(generateSchedule(MEMBER_1, LocalDateTime.of(2020, 8, 1, 6, 0), LocalDateTime.of(2020, 8, 1, 6, 30), "기상", "기상하기", LoopType.WEEK.getText()));
+        schedules.add(generateSchedule(MEMBER_1, LocalDateTime.of(2020, 9, 8, 6, 0), LocalDateTime.of(2020, 9, 8, 6, 30), "기상", "기상하기", LoopType.MONTH.getText()));
+        schedules.add(generateSchedule(MEMBER_1, LocalDateTime.of(2020, 9, 9, 6, 0), LocalDateTime.of(2020, 9, 9, 6, 30), "취침", "취침하기", LoopType.NONE.getText()));
+        schedules.add(generateSchedule(MEMBER_2, LocalDateTime.of(2020, 7, 10, 20, 0), LocalDateTime.of(2020, 7, 10, 21, 0), "취침", "취침하기", LoopType.NONE.getText()));
+        schedules.add(generateSchedule(MEMBER_2, LocalDateTime.of(2020, 7, 10, 20, 0), LocalDateTime.of(2020, 7, 10, 21, 0), "취침", "취침하기", LoopType.DAY.getText()));
+        schedules.add(generateSchedule(MEMBER_2, LocalDateTime.of(2020, 7, 17, 20, 0), LocalDateTime.of(2020, 7, 17, 21, 0), "취침", "취침하기", LoopType.WEEK.getText()));
+        schedules.add(generateSchedule(MEMBER_2, LocalDateTime.of(2020, 6, 10, 20, 0), LocalDateTime.of(2020, 6, 10, 21, 0), "취침", "취침하기", LoopType.MONTH.getText()));
+        schedules.add(generateSchedule(MEMBER_2, LocalDateTime.of(2020, 9, 10, 20, 0), LocalDateTime.of(2020, 9, 10, 21, 0), "기상", "기상하기", LoopType.WEEK.getText()));
+    }
+
+    private Schedule generateSchedule(long memberId, LocalDateTime startTime, LocalDateTime endTime, String category, String description, String loopType) {
+        Schedule schedule = Schedule.of(memberId, startTime, endTime, category, description, loopType);
+        try {
+        Class clazz = Class.forName("com.depromeet.domain.schedule.Schedule");
+        Field field = clazz.getDeclaredField("id");
+        field.setAccessible(true);
+        field.set(schedule, currentId++);
+        } catch (ReflectiveOperationException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return schedule;
+    }
+
+    @Override
+    public List<Schedule> getSchedulesByMemberIdAndLoopTypeAndYearAndMonthAndDay(long memberId, LoopType loopType, int year, int month, int day) {
+        return schedules
+            .stream()
+            .filter(s -> s.getMemberId() == memberId)
+            .filter(s -> s.getLoopType().equals(loopType))
+            .filter(s -> s.getYear() == year)
+            .filter(s -> s.getMonth() == month)
+            .filter(s -> s.getDay() == day)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Schedule> getSchedulesByMemberIdAndLoopType(long memberId, LoopType loopType) {
+        return schedules
+            .stream()
+            .filter(s -> s.getMemberId() == memberId)
+            .filter(s -> s.getLoopType().equals(loopType))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Schedule> getSchedulesByMemberIdAndLoopTypeAndDayOfWeek(long memberId, LoopType loopType, DayOfWeek dayOfWeek) {
+        return schedules
+            .stream()
+            .filter(s -> s.getMemberId() == memberId)
+            .filter(s -> s.getLoopType().equals(loopType))
+            .filter(s -> s.getDayOfWeek().equals(dayOfWeek))
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Schedule> getSchedulesByMemberIdAndLoopTypeAndDay(long memberId, LoopType loopType, int day) {
+        return schedules
+            .stream()
+            .filter(s -> s.getMemberId() == memberId)
+            .filter(s -> s.getLoopType().equals(loopType))
+            .filter(s -> s.getDay() == day)
+            .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Schedule> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Schedule> findAll(Sort sort) {
+        return null;
+    }
+
+    @Override
+    public List<Schedule> findAllById(Iterable<Long> longs) {
+        return null;
+    }
+
+    @Override
+    public <S extends Schedule> List<S> saveAll(Iterable<S> entities) {
+        return null;
+    }
+
+    @Override
+    public void flush() {
+
+    }
+
+    @Override
+    public <S extends Schedule> S saveAndFlush(S entity) {
+        return null;
+    }
+
+    @Override
+    public void deleteInBatch(Iterable<Schedule> entities) {
+
+    }
+
+    @Override
+    public void deleteAllInBatch() {
+
+    }
+
+    @Override
+    public Schedule getOne(Long aLong) {
+        return null;
+    }
+
+    @Override
+    public <S extends Schedule> List<S> findAll(Example<S> example) {
+        return null;
+    }
+
+    @Override
+    public <S extends Schedule> List<S> findAll(Example<S> example, Sort sort) {
+        return null;
+    }
+
+    @Override
+    public Page<Schedule> findAll(Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Schedule> S save(S entity) {
+        return null;
+    }
+
+    @Override
+    public Optional<Schedule> findById(Long aLong) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean existsById(Long aLong) {
+        return false;
+    }
+
+    @Override
+    public long count() {
+        return 0;
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+
+    }
+
+    @Override
+    public void delete(Schedule entity) {
+
+    }
+
+    @Override
+    public void deleteAll(Iterable<? extends Schedule> entities) {
+
+    }
+
+    @Override
+    public void deleteAll() {
+
+    }
+
+    @Override
+    public <S extends Schedule> Optional<S> findOne(Example<S> example) {
+        return Optional.empty();
+    }
+
+    @Override
+    public <S extends Schedule> Page<S> findAll(Example<S> example, Pageable pageable) {
+        return null;
+    }
+
+    @Override
+    public <S extends Schedule> long count(Example<S> example) {
+        return 0;
+    }
+
+    @Override
+    public <S extends Schedule> boolean exists(Example<S> example) {
+        return false;
+    }
+}
--- miracle-api/src/test/java/com/depromeet/service/schedule/ScheduleServiceTest.java
+++ miracle-api/src/test/java/com/depromeet/service/schedule/ScheduleServiceTest.java
@@ -1,19 +1,12 @@
 package com.depromeet.service.schedule;
 
-import com.depromeet.domain.schedule.LoopType;
-import com.depromeet.domain.schedule.Schedule;
-import com.depromeet.domain.schedule.ScheduleRepository;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.depromeet.service.schedule.dto.GetScheduleResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,40 +15,18 @@ import java.util.stream.Stream;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Schedule Service Test")
-@SpringBootTest
 class ScheduleServiceTest {
 
-    public static final long MEMBER_1 = 1L;
-    public static final long MEMBER_2 = 2L;
     private ScheduleService service;
 
-    @Autowired
-    private ScheduleRepository repository;
-
-    @BeforeEach
-    public void setUp() {
-        this.service = new ScheduleService(repository);
-        repository.save(Schedule.of(MEMBER_1, LocalDateTime.of(2020, 8, 8, 6, 0), LocalDateTime.of(2020, 8, 8, 6, 30), "기상", "기상하기", LoopType.NONE.getText()));
-        repository.save(Schedule.of(MEMBER_1, LocalDateTime.of(2020, 8, 8, 6, 0), LocalDateTime.of(2020, 8, 8, 6, 30), "기상", "기상하기", LoopType.DAY.getText()));
-        repository.save(Schedule.of(MEMBER_1, LocalDateTime.of(2020, 8, 1, 6, 0), LocalDateTime.of(2020, 8, 1, 6, 30), "기상", "기상하기", LoopType.WEEK.getText()));
-        repository.save(Schedule.of(MEMBER_1, LocalDateTime.of(2020, 9, 8, 6, 0), LocalDateTime.of(2020, 9, 8, 6, 30), "기상", "기상하기", LoopType.MONTH.getText()));
-        repository.save(Schedule.of(MEMBER_1, LocalDateTime.of(2020, 9, 9, 6, 0), LocalDateTime.of(2020, 9, 9, 6, 30), "취침", "취침하기", LoopType.NONE.getText()));
-        repository.save(Schedule.of(MEMBER_2, LocalDateTime.of(2020, 7, 10, 20, 0), LocalDateTime.of(2020, 7, 10, 21, 0), "취침", "취침하기", LoopType.NONE.getText()));
-        repository.save(Schedule.of(MEMBER_2, LocalDateTime.of(2020, 7, 10, 20, 0), LocalDateTime.of(2020, 7, 10, 21, 0), "취침", "취침하기", LoopType.DAY.getText()));
-        repository.save(Schedule.of(MEMBER_2, LocalDateTime.of(2020, 7, 17, 20, 0), LocalDateTime.of(2020, 7, 17, 21, 0), "취침", "취침하기", LoopType.WEEK.getText()));
-        repository.save(Schedule.of(MEMBER_2, LocalDateTime.of(2020, 6, 10, 20, 0), LocalDateTime.of(2020, 6, 10, 21, 0), "취침", "취침하기", LoopType.MONTH.getText()));
-        repository.save(Schedule.of(MEMBER_2, LocalDateTime.of(2020, 9, 10, 20, 0), LocalDateTime.of(2020, 9, 10, 21, 0), "기상", "기상하기", LoopType.WEEK.getText()));
-    }
-
-    @AfterEach
-    public void clean() {
-        repository.deleteAll();
+    public ScheduleServiceTest() {
+        this.service = new ScheduleService(new InMemoryScheduleRepository());
     }
 
     @DisplayName("하루 동안의 스케쥴을 조회 할 수 있다")
     @ParameterizedTest
     @MethodSource("source_getDailySchedule_ShouldSuccess")
-    public void getDailySchedule_ShouldSuccess(long memberId, LocalDate date, List<Long> scheduleIds) {
+    void getDailySchedule_ShouldSuccess(long memberId, LocalDate date, List<Long> scheduleIds) {
         List<GetScheduleResponse> response = service.getDailySchedule(memberId, date);
 
         List<Long> result = response
@@ -67,10 +38,10 @@ class ScheduleServiceTest {
         assertThat(scheduleIds.equals(result)).isTrue();
     }
 
-    public static Stream<Arguments> source_getDailySchedule_ShouldSuccess() {
+    static Stream<Arguments> source_getDailySchedule_ShouldSuccess() {
         return Stream.of(
-            Arguments.of(MEMBER_1, LocalDate.of(2020, 8, 8), Arrays.asList(1L, 2L, 3L, 4L)),
-            Arguments.of(MEMBER_2, LocalDate.of(2020, 7, 10), Arrays.asList(16L, 17L, 18L, 19L))
+            Arguments.of(InMemoryScheduleRepository.MEMBER_1, LocalDate.of(2020, 8, 8), Arrays.asList(1L, 2L, 3L, 4L)),
+            Arguments.of(InMemoryScheduleRepository.MEMBER_2, LocalDate.of(2020, 7, 10), Arrays.asList(6L, 7L, 8L, 9L))
         );
     }
 }
