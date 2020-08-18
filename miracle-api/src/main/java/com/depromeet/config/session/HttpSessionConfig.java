22d2b52cd59b888e6097c9223079d509991c498e
seungh0 <will.seungho@gmail.com>
2020-08-18 20:44:40 +0900

    [Miracle-13] 세션 만료 기간 설정 (15일)

--- miracle-api/src/main/java/com/depromeet/config/session/HttpSessionConfig.java
+++ miracle-api/src/main/java/com/depromeet/config/session/HttpSessionConfig.java
@@ -2,7 +2,7 @@ package com.depromeet.config.session;
 
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
-@EnableJdbcHttpSession
+@EnableJdbcHttpSession(maxInactiveIntervalInSeconds = 60 * 60 * 24 * 15)
 public class HttpSessionConfig {
 
 }
