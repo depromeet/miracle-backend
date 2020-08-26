540185e7180bc90a2745c362bcef0b85eb26a672
seungh0 <will.seungho@gmail.com>
2020-08-26 17:14:00 +0900

    fix: Record DDL 수정으로 인한 테스트 수정

--- miracle-domain/src/main/java/com/depromeet/domain/record/RecordCreator.java
+++ miracle-domain/src/main/java/com/depromeet/domain/record/RecordCreator.java
@@ -1,5 +1,6 @@
 package com.depromeet.domain.record;
 
+import com.depromeet.domain.common.Category;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -11,8 +12,12 @@ public class RecordCreator {
     public static Record create(Long memberId, LocalDateTime startDateTime, LocalDateTime endDateTime) {
         return Record.builder()
             .memberId(memberId)
+            .scheduleId(1L)
             .startDateTime(startDateTime)
             .endDateTime(endDateTime)
+            .category(Category.EXERCISE)
+            .question("question")
+            .answer("answer")
             .build();
     }
 
