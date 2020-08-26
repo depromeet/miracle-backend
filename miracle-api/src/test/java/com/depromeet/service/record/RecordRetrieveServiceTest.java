8536530a220b5ba5d2faa85e25818971209c7a0a
seungh0 <will.seungho@gmail.com>
2020-08-26 15:56:40 +0900

    refactor: 테스트 작게 분리 및 추가 + .http 수정

--- miracle-api/src/test/java/com/depromeet/service/record/RecordRetrieveServiceTest.java
+++ miracle-api/src/test/java/com/depromeet/service/record/RecordRetrieveServiceTest.java
@@ -1,12 +1,12 @@
 package com.depromeet.service.record;
 
-import com.depromeet.domain.record.Record;
 import com.depromeet.domain.record.RecordCreator;
 import com.depromeet.domain.record.RecordRepository;
 import com.depromeet.service.MemberSetup;
 import com.depromeet.service.record.dto.request.DayRecordsRequest;
 import com.depromeet.service.record.dto.request.MonthRecordsRequest;
 import com.depromeet.service.record.dto.response.RecordResponse;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +14,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -34,15 +33,13 @@ class RecordRetrieveServiceTest extends MemberSetup {
         recordRepository.deleteAll();
     }
 
+    @Description("7월 31일 23:50 ~ 8월 1일 00:10의 레코드 기록이 있을때, 8월의 레코드 정보를 불러오면 포함되어야 한다.")
     @Test
-    void 한달동안의_레코드를_불러온다_앞쪽에_겹치는경우() {
+    void 달의_레코드_조회_테스트_레코드가_앞쪽_경계에_걸쳐있는경우_포함되서_조회된다() {
         // given
-        List<Record> records = Arrays.asList(
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 7, 31, 0, 0, 0), LocalDateTime.of(2020, 7, 31, 23, 59, 59)),
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 7, 31, 0, 0, 0), LocalDateTime.of(2020, 8, 1, 12, 0, 0)),
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 8, 1, 0, 0, 0), LocalDateTime.of(2020, 8, 1, 12, 0, 0))
-        );
-        recordRepository.saveAll(records);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 7, 31, 23, 50);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 1, 0, 10);
+        recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         MonthRecordsRequest request = MonthRecordsRequest.testBuilder()
             .year(2020)
@@ -53,24 +50,17 @@ class RecordRetrieveServiceTest extends MemberSetup {
         List<RecordResponse> response = recordRetrieveService.retrieveMonthRecords(request, memberId);
 
         // then
-        assertThat(response).hasSize(2);
-        assertThat(response).extracting("startDateTime").containsExactly(
-            LocalDateTime.of(2020, 7, 31, 0, 0, 0),
-            LocalDateTime.of(2020, 8, 1, 0, 0, 0));
-        assertThat(response).extracting("endDateTime").containsExactly(
-            LocalDateTime.of(2020, 8, 1, 12, 0, 0),
-            LocalDateTime.of(2020, 8, 1, 12, 0, 0));
+        assertThat(response).hasSize(1);
+        assertRecordResponse(response.get(0), startDateTime, endDateTime);
     }
 
+    @Description("7월 31일 23:50 ~ 8월 1일 00:00 의 레코드 기록이 있을때, 8월의 레코드 정보를 불러오면 포함되지 않아야 한다.")
     @Test
-    void 한달동안의_레코드를_불러온다_끝에_겹치는경우() {
+    void 달의_레코드_조회_테스트_레코드가_앞쪽_경계에_겹치지_않는경우_포함되지_않는다() {
         // given
-        List<Record> records = Arrays.asList(
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 8, 30, 0, 0, 0), LocalDateTime.of(2020, 8, 30, 11, 59, 59)),
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 8, 31, 0, 0, 0), LocalDateTime.of(2020, 9, 1, 12, 0, 0)),
-            RecordCreator.create(memberId, LocalDateTime.of(2020, 9, 1, 0, 0, 0), LocalDateTime.of(2020, 9, 1, 12, 0, 0))
-        );
-        recordRepository.saveAll(records);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 7, 31, 23, 50);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 1, 0, 0);
+        recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         MonthRecordsRequest request = MonthRecordsRequest.testBuilder()
             .year(2020)
@@ -81,20 +71,56 @@ class RecordRetrieveServiceTest extends MemberSetup {
         List<RecordResponse> response = recordRetrieveService.retrieveMonthRecords(request, memberId);
 
         // then
-        assertThat(response).hasSize(2);
-        assertThat(response).extracting("startDateTime").containsExactly(
-            LocalDateTime.of(2020, 8, 30, 0, 0, 0),
-            LocalDateTime.of(2020, 8, 31, 0, 0, 0));
-        assertThat(response).extracting("endDateTime").containsExactly(
-            LocalDateTime.of(2020, 8, 30, 11, 59, 59),
-            LocalDateTime.of(2020, 9, 1, 12, 0, 0));
+        assertThat(response).isEmpty();
     }
 
+    @Description("8월 31일 23:50 ~ 9월 1일 00:10 의 레코드 기록이 있을때, 8월의 레코드 정보를 불러오면 포함되어야 한다.")
+    @Test
+    void 달의_레코드_조회_테스트_레코드가_뒷쪽_경계에_걸쳐있는경우_포함되서_조회된다() {
+        // given
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 31, 23, 50);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 9, 1, 0, 10);
+        recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
+
+        MonthRecordsRequest request = MonthRecordsRequest.testBuilder()
+            .year(2020)
+            .month(8)
+            .build();
+
+        // when
+        List<RecordResponse> response = recordRetrieveService.retrieveMonthRecords(request, memberId);
+
+        // then
+        assertThat(response).hasSize(1);
+        assertRecordResponse(response.get(0), startDateTime, endDateTime);
+    }
+
+    @Description("9월1일 00:00 ~ 9월 1일 00:10 의 레코드 기록이 있을때, 8월의 레코드 정보를 불러오면 포함되지 않아야한다.")
+    @Test
+    void 달의_레코드_조회_테스트_레코드가_뒷쪽_경계에_겹치지_않는경우_포함되지_않는다() {
+        // given
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 9, 1, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 9, 1, 0, 10);
+        recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
+
+        MonthRecordsRequest request = MonthRecordsRequest.testBuilder()
+            .year(2020)
+            .month(8)
+            .build();
+
+        // when
+        List<RecordResponse> response = recordRetrieveService.retrieveMonthRecords(request, memberId);
+
+        // then
+        assertThat(response).isEmpty();
+    }
+
+    @Description("8월 14일 00:00 ~ 15일 00:00의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되지 않아야한다.")
     @Test
     void 하루의_레코드를_불러오는기능_앞쪽에_겹치지_않는경우_포함되지_않는다() {
         // given
-        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 14, 0, 0, 0);
-        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 15, 0, 0, 0);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 14, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 15, 0, 0);
         recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
@@ -106,11 +132,12 @@ class RecordRetrieveServiceTest extends MemberSetup {
         assertThat(responses).isEmpty();
     }
 
+    @Description("8월 14일 00:00 ~ 15일 00:10의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되어야 한다.")
     @Test
     void 하루의_레코드를_불러오는기능_앞쪽에_겹치는_경우_포함된다() {
         // given
-        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 14, 0, 0, 0);
-        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 15, 1, 0, 0);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 14, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 15, 0, 10);
         recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
@@ -123,11 +150,12 @@ class RecordRetrieveServiceTest extends MemberSetup {
         assertRecordResponse(responses.get(0), startDateTime, endDateTime);
     }
 
+    @Description("8월 16일 00:00 ~ 16일 00:10의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되지 않아야 한다.")
     @Test
     void 하루의_레코드를_불러오는기능_뒷쪽에_겹치지_않는경우_포함되지_않는다() {
         // given
-        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 16, 0, 0, 0);
-        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 12, 0, 0);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 16, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 0, 10);
         recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
@@ -139,11 +167,12 @@ class RecordRetrieveServiceTest extends MemberSetup {
         assertThat(responses).isEmpty();
     }
 
+    @Description("8월 15일 23:50 ~ 16일 00:10의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되어야 한다.")
     @Test
     void 하루의_레코드를_불러오는기능_뒷쪽에_겹치는_경우_포함된다() {
         // given
-        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 15, 23, 0, 0);
-        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 1, 0, 0);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 15, 23, 50);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 1, 10);
         recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
@@ -156,11 +185,30 @@ class RecordRetrieveServiceTest extends MemberSetup {
         assertRecordResponse(responses.get(0), startDateTime, endDateTime);
     }
 
+    @Description("8월 15일 00:00 ~ 16일 00:00의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되어야 한다.")
     @Test
     void 하루의_레코드를_불러오는기_포함되서_겹치는경우_포함된다() {
         // given
-        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 15, 0, 0, 0);
-        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 15, 11, 59, 59);
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 15, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 0, 0);
+        recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
+
+        DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
+
+        // when
+        List<RecordResponse> responses = recordRetrieveService.retrieveDayRecords(request, memberId);
+
+        // then
+        assertThat(responses).hasSize(1);
+        assertRecordResponse(responses.get(0), startDateTime, endDateTime);
+    }
+
+    @Description("8월 14일 00:00 ~ 16일 00:00의 레코드 기록이 있을때, 8월 15일의 레코드 정보를 불러오면 포함되어야 한다.")
+    @Test
+    void 하루의_레코드를_불러오는기_아예_포함되어_지는경우_포함된다() {
+        // given
+        LocalDateTime startDateTime = LocalDateTime.of(2020, 8, 14, 0, 0);
+        LocalDateTime endDateTime = LocalDateTime.of(2020, 8, 16, 0, 0);
         recordRepository.save(RecordCreator.create(memberId, startDateTime, endDateTime));
 
         DayRecordsRequest request = DayRecordsRequest.testInstance(LocalDate.of(2020, 8, 15));
