b0ea5a6a9f08fe652606bcb6fe7436121f4af008
seungh0 <will.seungho@gmail.com>
2020-08-28 19:31:14 +0900

    fix: CalendarResponse LocalDate -> Day만 넘겨주도록 수정(yyyy-MM-dd -> dd)

--- miracle-api/src/main/java/com/depromeet/service/record/dto/response/RecordForCalendarResponse.kt
+++ miracle-api/src/main/java/com/depromeet/service/record/dto/response/RecordForCalendarResponse.kt
@@ -1,16 +1,14 @@
 package com.depromeet.service.record.dto.response
 
 import com.depromeet.domain.record.Record
-import java.time.LocalDate
-import java.util.*
 
 class RecordForCalendarResponse(
-    val dateList: List<LocalDate>
+    val dateList: List<Int>
 ) {
     companion object {
         @JvmStatic
         fun of(records: List<Record>): RecordForCalendarResponse {
-            return RecordForCalendarResponse(records.map { it.startDateTime.toLocalDate() }.toSortedSet().toList())
+            return RecordForCalendarResponse(records.map { it.startDateTime.dayOfMonth }.toSortedSet().toList())
         }
     }
 }
